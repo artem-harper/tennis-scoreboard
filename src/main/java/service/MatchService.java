063bbb4cfff28488bde1b23c56671608cec55c83
artikmay@mail.ru <artikmay@mail.ru>
2025-08-28 16:16:14 +0500

    added MatchScore

--- src/main/java/service/MatchService.java
+++ src/main/java/service/MatchService.java
@@ -1,17 +1,26 @@
 package service;
 
 import dto.PlayerDto;
+import entity.MatchScore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class MatchService {
 
     private final static MatchService INSTANCE = new MatchService();
 
-    private Map<UUID, >
+    private ConcurrentMap<UUID, MatchScore> matches = new ConcurrentHashMap<>();
 
     public void startNewMatch(PlayerDto player1, PlayerDto player2){
+        UUID MatchUUID = UUID.randomUUID();
 
+        MatchScore matchScore = new MatchScore(player1.getId(), player2.getId());
+
+        matches.put(MatchUUID, matchScore);
     }
 
     public static MatchService getInstance(){
